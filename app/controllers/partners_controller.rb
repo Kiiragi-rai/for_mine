931b0a1e4cbfff870dd5135319a7f8df56cf7e5e
rai-h-6 <laixixigu59@gmail.com>
2025-12-27 15:29:35 +0900

    partnerコントローラー#new,#updateを編集

--- app/controllers/partners_controller.rb
+++ app/controllers/partners_controller.rb
@@ -4,4 +4,28 @@ class PartnersController < ApplicationController
     def show
         @partner = current_user.partner
     end
+
+    def new 
+        @partner = Partner.new
+    end
+    def update
+        @partner = current_user.partner.build(partner_params)
+        if @partner.save
+            redirect_to partner_path, success: "OK"
+        else
+            flash.now[:danger] = "out"
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+
+
+    private
+
+    def partner_params
+        params.require(:partner).partmit(:name, :sex, :relation, :job, :favorites, :avoidances,:hobbies,
+        :budget_min,:budget_max)
+    end
 end
+
+

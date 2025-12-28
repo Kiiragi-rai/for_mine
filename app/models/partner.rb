8d41ea20cd4e0b8675d6fbbf38eb6c7c70321f0b
rai-h-6 <laixixigu59@gmail.com>
2025-12-29 03:33:13 +0900

    partnerモデル内整理

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -1,16 +1,15 @@
 class Partner < ApplicationRecord
 
+    belongs_to :user
 
     #一人につき一つ
-    validates :user_id, uniqueness: true 
+    validates :user_id, uniqueness: true
 
+    validates :name, presence: true
     validates :budget_max, numericality: { greater_than_or_equal_to: 0}, allow_nil: true
     validates :budget_min ,numericality: { greater_than_or_equal_to: 0}, allow_nil: true
 
     validate :budget_max_is_greater_than_budget_min
-
-
-    belongs_to :user
 
     # 予算（下限）が予算（上限）を超えないように
     def budget_max_is_greater_than_budget_min

258f1262620505de676198e767b12f79aa8ebc4e
rai-h-6 <laixixigu59@gmail.com>
2025-12-29 03:29:38 +0900

    削除の動線確保、partnerモデルの編集

--- app/models/partner.rb
+++ app/models/partner.rb
@@ -4,8 +4,8 @@ class Partner < ApplicationRecord
     #一人につき一つ
     validates :user_id, uniqueness: true 
 
-    validates :budget_max, numericality: { greater_than_or_equal_to: 0}
-    validates :budget_min ,numericality: { greater_than_or_equal_to: 0}
+    validates :budget_max, numericality: { greater_than_or_equal_to: 0}, allow_nil: true
+    validates :budget_min ,numericality: { greater_than_or_equal_to: 0}, allow_nil: true
 
     validate :budget_max_is_greater_than_budget_min
 
@@ -14,6 +14,7 @@ class Partner < ApplicationRecord
 
     # 予算（下限）が予算（上限）を超えないように
     def budget_max_is_greater_than_budget_min
+        return if budget_min.blank? || budget_max.blank?
         if budget_min >= budget_max
             errors.add(:budget_min,"予算（下限）が予算（上限）を超えることはできません")
         end

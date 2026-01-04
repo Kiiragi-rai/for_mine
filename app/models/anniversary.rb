040b219981256ad24bab378ddbae3173fe0961f0
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 18:12:19 +0900

    対象データ取得スコープを作成

--- app/models/anniversary.rb
+++ app/models/anniversary.rb
@@ -1,4 +1,6 @@
 class Anniversary < ApplicationRecord
     validates :title, presence:  { message: "を入力してね" }
     validates :anniversary_date, presence:  { message: "を入力してね" }
+
+    scope :notification_target_get, -> (date){ where(notification_on: true).where(anniversary_date: date)}  
 end

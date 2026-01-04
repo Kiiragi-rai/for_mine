2f1580847845249530dbf756da800f6bbe5e2389
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 17:09:28 +0900

    通知ON　OFF設定 カラム追加,viewを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_02_092508) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_04_075742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_02_092508) do
     t.date "anniversary_date", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "notification_on", default: false, null: false
     t.index ["user_id"], name: "index_anniversaries_on_user_id"
   end
 

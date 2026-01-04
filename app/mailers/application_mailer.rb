9c7a0b3a348319261d0a8d353e2b6c0c310b34ea
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 19:09:37 +0900

    Mailer作成、開発環境で送るように

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,6 @@
 class ApplicationMailer < ActionMailer::Base
+  #送信元メールアドレス
   default from: "from@example.com"
+  # メール全体に適用されるメールフォーム（Viewsのapplication.html.hamlなどと共通の原理かと）
   layout "mailer"
 end
---
+++ app/mailers/notification_mailer.rb
@@ -0,0 +1,13 @@
+class NotificationMailer < ApplicationMailer
+  # attr_accessor :email,:title
+  #仮引数に@付きのインスタンス変数は指定できませんので注意
+  def send_notification_mail(email,title)
+     #インスタンス変数に格納
+    @email = email
+    @title = title
+     #メールの宛先を指定
+      #メールのタイトルを指定
+    mail to: @email,
+         subject: "[記念日]#{@title}"
+  end
+end

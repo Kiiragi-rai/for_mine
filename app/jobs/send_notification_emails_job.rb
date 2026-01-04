b8d4cfabfc951f71485ebb3d4b62add5437f4cff
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 20:26:13 +0900

    メール送信メッセージの内容訂正

--- app/jobs/send_notification_emails_job.rb
+++ app/jobs/send_notification_emails_job.rb
@@ -4,17 +4,23 @@ class SendNotificationEmailsJob < ApplicationJob
   def perform
     # Do something later
     target_today = Date.today
-    p target_today 
+    Rails.logger.info "[SendNotificationEmailsJob] target_today=#{target_today}"
+
     @anniversaries = Anniversary.notification_target_get(target_today)
-    p @anniversaries
+    Rails.logger.info "[SendNotificationEmailsJob] anniversaries_count=#{@anniversaries.size}"
+    Rails.logger.info "[SendNotificationEmailsJob] anniversaries_ids=#{@anniversaries.pluck(:id).join(',')}"
+
     @anniversaries.each do |anniversary|
       email = anniversary.user.email
-      your_anniversary_date = anniversary.anniversary_date
-      
-      p email
-      p your_anniversary_date
+      your_anniversary_title = anniversary.title
+      Rails.logger.info "[SendNotificationEmailsJob] sending to=#{email} anniversary_id=#{anniversary.id} anniversary_date=#{your_anniversary_title}"
 
-      NotificationMailer.send_notification_mail(email,your_anniversary_date).deliver_now
+
+      NotificationMailer.send_notification_mail(email,your_anniversary_title).deliver_now
+      Rails.logger.info "[SendNotificationEmailsJob] sending to=#{email} anniversary_id=#{anniversary.id} anniversary_date=#{your_anniversary_title}"
+
     end
+    Rails.logger.info "[SendNotificationEmailsJob] done"
+
   end
 end

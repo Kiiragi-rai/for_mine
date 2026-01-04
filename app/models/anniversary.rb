a01e8fdfc8914cb3c0b37294a56e7d0093656ca3
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 18:37:46 +0900

    gemfile追加

--- app/models/anniversary.rb
+++ app/models/anniversary.rb
@@ -1,3 +1,23 @@
+# == Schema Information
+#
+# Table name: anniversaries
+#
+#  id               :bigint           not null, primary key
+#  anniversary_date :date             not null
+#  notification_on  :boolean          default(FALSE), not null
+#  title            :string           not null
+#  created_at       :datetime         not null
+#  updated_at       :datetime         not null
+#  user_id          :bigint           not null
+#
+# Indexes
+#
+#  index_anniversaries_on_user_id  (user_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (user_id => users.id)
+#
 class Anniversary < ApplicationRecord
     validates :title, presence:  { message: "を入力してね" }
     validates :anniversary_date, presence:  { message: "を入力してね" }

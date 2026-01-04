aa33a223d5bb5bf9cc5d664fe91ade9a5c93be9e
rai-h-6 <laixixigu59@gmail.com>
2026-01-04 19:38:09 +0900

    anniversaryにbelongsを追加

--- app/models/anniversary.rb
+++ app/models/anniversary.rb
@@ -19,6 +19,7 @@
 #  fk_rails_...  (user_id => users.id)
 #
 class Anniversary < ApplicationRecord
+    belongs_to :user
     validates :title, presence:  { message: "を入力してね" }
     validates :anniversary_date, presence:  { message: "を入力してね" }
 

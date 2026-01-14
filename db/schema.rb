3290f9d18c19cbaaa2e7ffe592d241a7743382a6
rai-h-6 <laixixigu59@gmail.com>
2026-01-14 17:04:58 +0900

    LINEログイン用のuserカラム調整、user モデル調整

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_01_04_075742) do
+ActiveRecord::Schema[7.2].define(version: 2026_01_14_075842) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,16 +42,12 @@ ActiveRecord::Schema[7.2].define(version: 2026_01_04_075742) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
     t.string "name", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.string "provider", default: "", null: false
+    t.string "uid", default: "", null: false
+    t.index ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true
   end
 
   add_foreign_key "anniversaries", "users"

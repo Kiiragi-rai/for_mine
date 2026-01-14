a2b5f5d9011c7a2f8de4de7007afd2d36da08b5d
rai-h-6 <laixixigu59@gmail.com>
2026-01-14 20:20:52 +0900

    rememberable削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@ class User < ApplicationRecord
   validates :provider, uniqueness: { scope: :uid }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise  :rememberable,:omniauthable, omniauth_providers: [:line]
+  devise  :omniauthable, omniauth_providers: [:line]
 
   has_many :anniversaries, dependent: :destroy
   has_one :partner, dependent: :destroy

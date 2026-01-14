f4af3a3ba2206f3d706b5b8d6b2fa763df580cb7
rai-h-6 <laixixigu59@gmail.com>
2026-01-14 19:34:51 +0900

    routes調整、コントローラー訂正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,12 +1,7 @@
 # frozen_string_literal: true
 
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-  # You should configure your model like this:
-  # devise :omniauthable, omniauth_providers: [:twitter]
 
-  # You should also create an action method in this controller like this:
-  # def twitter
-  # end
   def line
     @user = User.from_line(request.env["omniauth.auth"])
 
@@ -29,23 +24,5 @@ def after_omniauth_failure_path_for(_scope)
   root_path
 end
 end
-  # More info at:
-  # https://github.com/heartcombo/devise#omniauth
 
-  # GET|POST /resource/auth/twitter
-  # def passthru
-  #   super
-  # end
-
-  # GET|POST /users/auth/twitter/callback
-  # def failure
-  #   super
-  # end
-
-  # protected
-
-  # The path used when OmniAuth fails
-  # def after_omniauth_failure_path_for(scope)
-  #   super(scope)
-  # end
 

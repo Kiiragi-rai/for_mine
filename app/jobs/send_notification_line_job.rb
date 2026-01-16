6f4979de57d391efd06fd69b00ff2aee11a29e6c
rai-h-6 <laixixigu59@gmail.com>
2026-01-16 22:47:57 +0900

    本番用

--- app/jobs/send_notification_line_job.rb
+++ app/jobs/send_notification_line_job.rb
@@ -1,54 +1,25 @@
 class SendNotificationLineJob < ApplicationJob
  queue_as :default
 
- DEFAULT_TEXT = "今日が記念日だぜ"
+ DEFAULT_TEXT = "今日が記念日!"
 
   def perform(text_messages: DEFAULT_TEXT)
     # Do something later
     target_today = Date.today
     anniversaries = Anniversary.notification_target_get(target_today)
     Rails.logger.info "@annversariesの中身 #{anniversaries}"
-    # @anniversaries.each do |anniversary|
-    #   # uid = anniversary.user.uid
 
+   
     return if anniversaries.blank?
-     LineNotification::LineClient.send_message(text_messages: DEFAULT_TEXT)
-      # LineNotification::LineClient.send_message(uid,text_messages)
-  end
 
+    anniversaries.each do |anniversary|
+      uid = anniversary.user.uid
+
+
+      next if uid.blank?
+
+      LineNotification::LineClient.send_message(uid: uid,text_messages: DEFAULT_TEXT)
+    end
+    end
 end
  
-
-
-
-module LineNotification
-  class LineClient
-    DEFAULT_TEXT = "今日が記念日だぜ"
-
-    # def send_message(_uid, message_contents: DEFAULT_TEXT)
-    def self.send_message(text_messages: DEFAULT_TEXT)
-      push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
-    to: ENV.fetch("UID"),
-    messages: [
-      Line::Bot::V2::MessagingApi::TextMessage.new(text: text_messages)
-    ]
-  )
-    # メッセージ送信
-    _response, status, _headers = client.push_message_with_http_info(
-      push_message_request: push_request
-    )
-    status == 200
-  
-    Rails.logger.info "[LINENOTICATION:LINECLIENT] status=#{status}"
-
-    end
-
-    private
-    def client
-      @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
-        channel_access_token: ENV.fetch("LINE_CHANNEL_ACCESS_TOKEN")
-      )
-    end
-
-end
-end
--- app/services/line_notification/line_client.rb
+++ app/services/line_notification/line_client.rb
@@ -3,9 +3,9 @@ module LineNotification
       DEFAULT_TEXT = "今日が記念日だぜ"
 
       # def send_message(_uid, message_contents: DEFAULT_TEXT)
-      def self.send_message(text_messages: DEFAULT_TEXT)
+      def self.send_message(uid:,text_messages: DEFAULT_TEXT)
         push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
-      to: ENV.fetch("UID"),
+      to: uid,
       messages: [
         Line::Bot::V2::MessagingApi::TextMessage.new(text: text_messages)
       ]
@@ -15,15 +15,13 @@ module LineNotification
         push_message_request: push_request
       )
       status == 200
-      profile = client.get_profile(user_id: ENV.fetch("UID"))
 
       Rails.logger.info "[LINENOTICATION:LINECLIENT] status=#{status}"
-      Rails.logger.info "[Line] profile display_name=#{profile.display_name}"
-
 
       end
 
       private
+
       def self.client
         @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
           channel_access_token: ENV.fetch("LINE_MESSAGING_CHANNEL_ACCESS_TOKEN")

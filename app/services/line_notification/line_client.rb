6bcd56058657db3c994ce75c6806b1dc29578133
rai-h-6 <laixixigu59@gmail.com>
2026-01-16 21:41:09 +0900

    service編集

--- app/services/line_notification/line_client.rb
+++ app/services/line_notification/line_client.rb
@@ -1,25 +1,22 @@
 module LineNotification
-  class LineClient
-    def send_message(uid, message_contents)
-      push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
-    to: uid,
-    messages: [
-      Line::Bot::V2::MessagingApi::TextMessage.new(text: message_contents)
-    ]
-  )
+    class LineClient
+      DEFAULT_TEXT = "今日が記念日だぜ"
+      def send_message(_uid, message_contents: DEFAULT_TEXT)
+        push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
+      to: ENV.fetch("UID"),
+      messages: [
+        Line::Bot::V2::MessagingApi::TextMessage.new(text: message_contents)
+      ]
+    )
 
-  # メッセージ送信
-  _response, status, _headers = client.push_message_with_http_info(
-    push_message_request: push_request
-  )
-  status == 200
-  end
-    
-
-  
+      # メッセージ送信
+      _response, status, _headers = client.push_message_with_http_info(
+        push_message_request: push_request
+      )
+      status == 200
+      end
 
   private 
-
       def client
       @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
         channel_access_token: ENV.fetch("LINE_CHANNEL_ACCESS_TOKEN")

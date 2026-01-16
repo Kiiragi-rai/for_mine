29a8303ccf9c5e0e581373da6f5e95e6a6a90faf
rai-h-6 <laixixigu59@gmail.com>
2026-01-16 22:37:25 +0900

    ローカルでのメッセージ確認

--- app/services/line_notification/line_client.rb
+++ app/services/line_notification/line_client.rb
@@ -1,26 +1,34 @@
 module LineNotification
     class LineClient
       DEFAULT_TEXT = "今日が記念日だぜ"
-      def send_message(_uid, message_contents: DEFAULT_TEXT)
+
+      # def send_message(_uid, message_contents: DEFAULT_TEXT)
+      def self.send_message(text_messages: DEFAULT_TEXT)
         push_request = Line::Bot::V2::MessagingApi::PushMessageRequest.new(
       to: ENV.fetch("UID"),
       messages: [
-        Line::Bot::V2::MessagingApi::TextMessage.new(text: message_contents)
+        Line::Bot::V2::MessagingApi::TextMessage.new(text: text_messages)
       ]
     )
-
       # メッセージ送信
       _response, status, _headers = client.push_message_with_http_info(
         push_message_request: push_request
       )
       status == 200
+      profile = client.get_profile(user_id: ENV.fetch("UID"))
+
+      Rails.logger.info "[LINENOTICATION:LINECLIENT] status=#{status}"
+      Rails.logger.info "[Line] profile display_name=#{profile.display_name}"
+
+
       end
 
-  private 
-      def client
-      @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
-        channel_access_token: ENV.fetch("LINE_CHANNEL_ACCESS_TOKEN")
-      )
-    end
+      private
+      def self.client
+        @client ||= Line::Bot::V2::MessagingApi::ApiClient.new(
+          channel_access_token: ENV.fetch("LINE_MESSAGING_CHANNEL_ACCESS_TOKEN")
+        )
+      end
+
   end
 end

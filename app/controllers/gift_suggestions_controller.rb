4d0fb453b4f82f546a688a6d582270bcf9641603
rai-h-6 <laixixigu59@gmail.com>
2026-01-18 18:59:22 +0900

    動作確認ように変更

--- app/controllers/gift_suggestions_controller.rb
+++ app/controllers/gift_suggestions_controller.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class GiftSuggestionsController < ApplicationController
   before_action :authenticate_user!
 
@@ -5,26 +7,33 @@ class GiftSuggestionsController < ApplicationController
   def new
     @contens = nil
   end
+    「#{ partner_info }」を元におすすめプレゼントとそれを提案した理由をして下さい。
+  #  関係性は#{partner.relation},年齢は#{partner.age},好きなものは#{partner.likes.join(",")}で、これを元に
+  #   お薦めのプレゼントとその理由を教えてください
+  #   以下のJSON形式で、キーや値の型も完全に守って応答してください。
 
   def create
-    @partner_info = current_user.partner
-    Rails.logger.info "#{@partner_info}"
-    prompt = <<-PROMPT
-    「#{@partner_info}」を元におすすめプレゼントとそれを提案した理由をして下さい。
-    以下のJSON形式で、キーや値の型も完全に守って応答してください。
+    partner = current_user.partner
 
+    Rails.logger.info "#{partner_info}"
+    prompt <<-PROMPT
 
+    男性向けのプレゼントを２つ提案してください。あとその理由も。
+  
+
+ 
     {
-    "presentSuggestion": [
-    { "suggestion1": "プレゼント提案1", "suggention1Reason": "プレゼント提案１の理由"},
-    { "suggestion2": "プレゼント提案2", "suggention2Reason": "プレゼント提案2の理由"}
-    ]
+      "presentSuggestion": [
+        { "name": "プレゼント提案1", "reason": "プレゼント提案１の理由"},
+        { "name": "プレゼント提案2", "reason": "プレゼント提案2の理由"}
+       ]
     }
     PROMPT
 
-    @contents = GiftSuggestions::Generate.new(@prompt).call
+   
+
+    @contents = GiftSuggestions::Generate.new(prompt).call
 
     render :new, status: :ok
   end
 end
-
--- app/services/gift_suggestions/gererate.rb
+++ app/services/gift_suggestions/gererate.rb
@@ -1,22 +1,21 @@
 module GiftSuggestions
   class Generate
-    
     def initializer(prompt)
       @prompt = prompt
       Rails.logger.info "promptの中身だよん#{@prompt}"
     end
 
-    def call 
+    def call
       client = OpenAI::Client.new
-      
+
       response = client.chat(
         parameters: {
           model: "gpt-4o-mini",
-          messages: [{ role: "user", content: @prompt}],
+          messages: [ { role: "user", content: @prompt } ],
 
           response_format: { type: "json_object" },
 
-          temperature: 0.7,
+          temperature: 0.7
         }
       )
 

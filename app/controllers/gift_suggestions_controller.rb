be508689aeb99884261c3f75fc85ca91058c1945
rai-h-6 <laixixigu59@gmail.com>
2026-01-18 22:43:15 +0900

    コントローラー微調整

--- app/controllers/gift_suggestions_controller.rb
+++ app/controllers/gift_suggestions_controller.rb
@@ -9,40 +9,40 @@ class GiftSuggestionsController < ApplicationController
   end
 
   def create
-    # if Rails.env.production?
-    # partner = current_user.partner
+        # if Rails.env.production?
+        # partner = current_user.partner
 
-    #  partner_info = {
-    #   sex: partner.sex.presence || "未入力",
-    #   age: partner.age.presence || "未入力",
-    #   job: partner.job.presence || "未入力",
-    #   relation: partner.relation.presence || "未入力",
-    #   budget_min: partner.with_yen(partner.budget_min),
-    #   budget_max: partner.with_yen(partner.budget_max),
-    #   favorites: partner.turn_to_string(partner.favorites),
-    #   avoidances: partner.turn_to_string(partner.avoidances),
-    #   hobbies: partner.turn_to_string(partner.hobbies)
-    # }
+        #  partner_info = {
+        #   sex: partner.sex.presence || "未入力",
+        #   age: partner.age.presence || "未入力",
+        #   job: partner.job.presence || "未入力",
+        #   relation: partner.relation.presence || "未入力",
+        #   budget_min: partner.with_yen(partner.budget_min),
+        #   budget_max: partner.with_yen(partner.budget_max),
+        #   favorites: partner.turn_to_string(partner.favorites),
+        #   avoidances: partner.turn_to_string(partner.avoidances),
+        #   hobbies: partner.turn_to_string(partner.hobbies)
+        # }
 
-    # prompt = <<-PROMPT
+        # prompt = <<-PROMPT
 
-    # #{partner_info.to_json}を元におすすめのプレゼント3つとその理由を教えてください。
-    # 以下のJSON形式で、キーや値の型も完全に守って応答してください。
+        # #{partner_info.to_json}を元におすすめのプレゼント3つとその理由を教えてください。
+        # 以下のJSON形式で、キーや値の型も完全に守って応答してください。
 
-    # {
-    #   "presentSuggestions": [
-    #     { "name": "プレゼント提案1", "reason": "プレゼント提案１の理由"},
-    #     { "name": "プレゼント提案2", "reason": "プレゼント提案2の理由"},
-    #     { "name": "プレゼント提案3", "reason": "プレゼント提案3の理由"}
-    #    ]
-    # }
-    # PROMPT
-
-
-    # @contents = GiftSuggestions::Generate.new(prompt).call
+        # {
+        #   "presentSuggestions": [
+        #     { "name": "プレゼント提案1", "reason": "プレゼント提案１の理由"},
+        #     { "name": "プレゼント提案2", "reason": "プレゼント提案2の理由"},
+        #     { "name": "プレゼント提案3", "reason": "プレゼント提案3の理由"}
+        #    ]
+        # }
+        # PROMPT
 
 
-    # elsif Rails.env.development?
+        # @contents = GiftSuggestions::Generate.new(prompt).call
+
+
+        # elsif Rails.env.development?
         @contents = {
       "presentSuggestions" => [
         { "name" => "文房具セット", "reason" => "..." },

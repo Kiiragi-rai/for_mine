97631d5db05a1cf5351f712dd8fe6e270ec69420
rai-h-6 <laixixigu59@gmail.com>
2026-01-28 16:24:55 +0900

    提案機能改善　過去に結果があれば被らないように

--- app/controllers/gift_suggestions_controller.rb
+++ app/controllers/gift_suggestions_controller.rb
@@ -10,39 +10,52 @@ class GiftSuggestionsController < ApplicationController
 
   def create
         # if Rails.env.production?
-        # partner = current_user.partner
+        partner = current_user.partner
 
-        #  partner_info = {
-        #   sex: partner.sex.presence || "未入力",
-        #   age: partner.age.presence || "未入力",
-        #   job: partner.job.presence || "未入力",
-        #   relation: partner.relation.presence || "未入力",
-        #   budget_min: partner.with_yen(partner.budget_min),
-        #   budget_max: partner.with_yen(partner.budget_max),
-        #   favorites: partner.turn_to_string(partner.favorites),
-        #   avoidances: partner.turn_to_string(partner.avoidances),
-        #   hobbies: partner.turn_to_string(partner.hobbies)
-        # }
+         partner_info = {
+          sex: partner.sex.presence || "未入力",
+          age: partner.age.presence || "未入力",
+          job: partner.job.presence || "未入力",
+          relation: partner.relation.presence || "未入力",
+          budget_min: partner.with_yen(partner.budget_min),
+          budget_max: partner.with_yen(partner.budget_max),
+          favorites: partner.turn_to_string(partner.favorites),
+          avoidances: partner.turn_to_string(partner.avoidances),
+          hobbies: partner.turn_to_string(partner.hobbies)
+        }
 
-        # prompt = <<-PROMPT
+        # ここでinfo を保存と渡す
+        # prompt分岐
 
-        # #{partner_info.to_json}を元におすすめのプレゼント3つとその理由を教えてください。
-        # 以下のJSON形式で、キーや値の型も完全に守って応答してください。
+        # Rails.logger.info "#{target.input_json}"
 
-        # {
-        #   "presentSuggestions": [
-        #     { "name": "プレゼント提案1", "reason": "プレゼント提案１の理由"},
-        #     { "name": "プレゼント提案2", "reason": "プレゼント提案2の理由"},
-        #     { "name": "プレゼント提案3", "reason": "プレゼント提案3の理由"}
-        #    ]
-        # }
-        # PROMPT
+        prompt = <<-PROMPT
 
+        #{partner_info.to_json}を元におすすめのプレゼント3つとその理由を教えてください。
+        以下のJSON形式で、キーや値の型も完全に守って応答してください。
+
+        {
+          "presentSuggestions": [
+            { "name": "プレゼント提案1", "reason": "プレゼント提案１の理由"},
+            { "name": "プレゼント提案2", "reason": "プレゼント提案2の理由"},
+            { "name": "プレゼント提案3", "reason": "プレゼント提案3の理由"}
+           ]
+        }
+        PROMPT
+
+        last_result =  current_user.gift_suggestions&.last&.result_json  
+        names = last_result&.dig("presentSuggestions")&.map { |h| h["name"] }
+
+        if last_result.present?
+        prompt << "\n #{names.to_json}は避けてください" 
+        end
+
+        # names = last_result["presentSuggestions"]&.map { ["name"] }&.join(", ")
+        prompt << "\n 次のプレゼント名は避けてください: #{names}" if names.present?
 
         # @contents = GiftSuggestions::Generate.new(prompt).call
 
-
-        # elsif Rails.env.development?
+        # elsif Rails.env.development?  
         @contents = {
       "presentSuggestions" => [
         { "name" => "文房具セット", "reason" => "..." },
@@ -50,9 +63,14 @@ class GiftSuggestionsController < ApplicationController
         { "name" => "オリジナルのメッセージカード", "reason" => "..." }
       ]
     }
-    # end
 
-    session[:gift_contents] = @contents
+    target = current_user.gift_suggestions.build(input_json: partner_info,result_json: @contents)
+    if target.save
+   session[:gift_contents] = @contents
     redirect_to new_gift_suggestion_path, notice: "提案を生成しました"
+    else
+      render :new, status: :unprocessable_entity
+    end
+    # end
   end
 end

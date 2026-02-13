74b4daa34dcaa85a9a247a0612f4a25f7cef3c3f
rai-h-6 <laixixigu59@gmail.com>
2026-02-14 01:24:11 +0900

    処理抜け追加

--- app/services/line_notification/notification_get.rb
+++ app/services/line_notification/notification_get.rb
@@ -5,13 +5,13 @@ module LineNotification
         sql= <<~SQL
 
           WITH base AS (
-            SELECT#{' '}
+            SELECT
               date_trunc(
                 'hour',
-                now() + interval '1 hour'#{' '}
+                now() + interval '1 hour'
               )AS next_hour
           )
-        SELECT#{' '}
+        SELECT
           u.id AS user_id,
           ns.id AS notification_setting_id,
           ns.start_on,
@@ -20,30 +20,37 @@ module LineNotification
           b.next_hour AS scheduled_for
 
           FROM anniversaries a
-          INNER JOIN users u#{' '}
+          INNER JOIN users u
           ON a.user_id = u.id
 
-          INNER JOIN notification_settings ns#{' '}
+          INNER JOIN notification_settings ns
           ON a.id = ns.anniversary_id
 
           CROSS JOIN base b
 
-          LEFT JOIN notification_managements nm#{' '}
+          LEFT JOIN notification_managements nm
           ON ns.id = nm.notification_setting_id
           AND nm.scheduled_for = b.next_hour
 
-          WHERE#{' '}
+          
+
+          WHERE
             ns.is_enabled = true
+
             AND
-            nm.id IS NULL#{' '}
+            nm.id IS NULL
+
+            AND
+            (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date between ns.start_on and ns.end_on
+
             AND
             ns.notification_time = b.next_hour::time
 
-            AND#{' '}
-            (ns.last_sent_on IS NULL#{' '}
-            OR#{' '}
+            AND
+            (ns.last_sent_on IS NULL
+            OR
             ns.frequency_days <= (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date - ns.last_sent_on )
-        #{'    '}
+       
             ORDER BY ns.id, u.id
 
       SQL

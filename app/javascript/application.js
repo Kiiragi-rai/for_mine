b6bea55916c4dfe2fc51dbd2fcabbd88c3d1d1f4
rai-h-6 <laixixigu59@gmail.com>
2026-02-21 20:43:19 +0900

    使い方ページにフェードインを使用

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -35,3 +35,50 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = url.toString();
     });
   });
+
+
+  // fade-in 　how_toで使用
+  const scrollFade = () => {
+
+    const fadeElem = document.querySelectorAll(".ef");
+    const elemLength = fadeElem.length;
+    for (let i = 0; i < elemLength; i++) {
+      fadeElem[i].style.opacity = 0;
+    }
+  
+    window.addEventListener("scroll", (event) => {
+      fadeElem.forEach((fadeElem) => {
+        const ePos = fadeElem.offsetTop;
+        const scroll = window.scrollY;
+        const windowHeight = window.innerHeight;
+        if (scroll > ePos - windowHeight + windowHeight/5){
+          fadeElem.style.opacity = 1;
+        }
+      });
+    });
+  
+  }
+  scrollFade();
+
+
+  // fade -in 2
+  //要素をフェードインする処理
+const showElements = () => {
+  //フェードインする要素を全て取得
+  const elements = document.querySelectorAll(".fadein");
+  //ブラウザの高さの80%を計算
+  const displayPos = window.innerHeight * 0.8;
+  
+  elements.forEach((element) => {
+    //ブラウザの上から要素の上までの距離
+    const elementPos = element.getBoundingClientRect().top;
+   //要素がブラウザの下から20%の位置よりも上に到達したら実行する
+    if(displayPos > elementPos) {
+      //要素に「show」クラスを追加
+      element.classList.add("show");
+    }
+  });
+};
+
+window.addEventListener("load", showElements);
+window.addEventListener("scroll", showElements);

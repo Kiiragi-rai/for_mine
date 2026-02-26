b9d170aaef3e1bcefe15aca74fbc2e2dd0b62ade
rai-h-6 <laixixigu59@gmail.com>
2026-02-26 20:45:49 +0900

    プレゼント提案待ち時間ローディング完成

--- app/javascript/controllers/gift_loading_controller.js
+++ app/javascript/controllers/gift_loading_controller.js
@@ -2,20 +2,24 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="gift-loading"
 export default class extends Controller {
-  connect() {
+  static targets = ["submitButton", "loadingIndicator"]
+  submit() {
+    this.submitButtonTarget.disabled = true
+    this.submitButtonTarget.value = "考え中..."
+    this.loadingIndicatorTarget.classList.remove("hidden")
   }
 }
-document.addEventListener("DOMContentLoaded", () => {
-  const form = document.querySelector("form");
-  const submitButton = document.getElementById("gift-submit-button");
-  const loadingIndicator = document.getElementById("loading-indicator");
+// document.addEventListener("DOMContentLoaded", () => {
+//   const form = document.querySelector("form");
+//   const submitButton = document.getElementById("gift-submit-button");
+//   const loadingIndicator = document.getElementById("loading-indicator");
 
-  if (form) {
-    form.addEventListener("submit", () => {
-      submitButton.disabled = true;
-      submitButton.value = "考え中...";
-      loadingIndicator.classList.remove("hidden");
-    });
-  }
-});
-
+//   if (form) {
+//     form.addEventListener("submit", () => {
+//       submitButton.disabled = true;
+//       submitButton.value = "考え中...";
+//       loadingIndicator.classList.remove("hidden");
+//     });
+//   }
+// });
+// l

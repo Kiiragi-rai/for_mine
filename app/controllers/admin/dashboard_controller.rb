eed4cb1893546342574c2ca725fa62b6daa8387c
rai-h-6 <laixixigu59@gmail.com>
2026-02-27 22:05:17 +0900

    admin/users微調整

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,7 @@
 class Admin::DashboardController < Admin::BaseController
+  before_action :authenticate_admin!
+
   def index
+    @users = User.all.count
   end
 end
--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,6 @@
-class Admin::UsersController < ApplicationController
+class Admin::UsersController < Admin::BaseController
+  before_action :authenticate_admin!
+
   def index
     @users = User.order(created_at: :desc)
   end

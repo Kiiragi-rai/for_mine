431779c93aa5f220b30fa617fe719f01bda157bd
rai-h-6 <laixixigu59@gmail.com>
2026-03-01 16:30:08 +0900

    管理画面入場せいげん

--- app/controllers/admin/notification_managements_controller.rb
+++ app/controllers/admin/notification_managements_controller.rb
@@ -1,4 +1,6 @@
 class Admin::NotificationManagementsController < Admin::BaseController
+  before_action :authenticate_admin!
+
   def index
     @notification_managements = NotificationManagement.order(created_at: :desc)
   end

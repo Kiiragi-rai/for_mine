d3e9145107d430b83198a257aac37c70560f0c90
rai-h-6 <laixixigu59@gmail.com>
2026-03-06 18:08:14 +0900

    記念日通知設定修正

--- app/models/anniversary.rb
+++ app/models/anniversary.rb
@@ -27,6 +27,7 @@ class Anniversary < ApplicationRecord
     validates :title, presence:  { message: "を入力してね" }
     validates :anniversary_date, presence:  { message: "を入力してね" }
 
+
     scope :notification_target_get, ->(date) { where(notification_on: true).where(anniversary_date: date) }
 
 
@@ -71,6 +72,8 @@ class Anniversary < ApplicationRecord
       end
         this_year
     end
+
+
 
 
     def start_time

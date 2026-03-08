9a1248b8f614a24d97bc7dcf19728f69316e41be
rai-h-6 <laixixigu59@gmail.com>
2026-03-08 23:38:49 +0900

    ヘルス

---
+++ app/controllers/health_controller.rb
@@ -0,0 +1,8 @@
+class HealthController < ApplicationController
+  skip_before_action :require_login
+  def show
+    db_ok = ActiveRecord::Base.connection.active? rescue false
+    redis_ok = Redis.new(url: ENV.fetch("REDIS_URL", "redis://redis:6379/1")).ping == "PONG" rescue false
+    render json: { ok: db_ok && redis_ok, db: db_ok, redis: redis_ok }
+  end
+end

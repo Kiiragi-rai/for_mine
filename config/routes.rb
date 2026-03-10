81983e885bbc7eb0a10b2ec14072dcc54819691f
rai-h-6 <laixixigu59@gmail.com>
2026-03-10 22:44:46 +0900

    admin giftsuggestioncontr追加

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
     root "dashboard#index"
     resources :users, only: [ :index ]
     resources :notification_managements, only: [ :index ]
+    resources :gift_suggestions, only: [:index]
+end
   end
   authenticate :admin do
     mount Sidekiq::Web => "/admin/sidekiq"

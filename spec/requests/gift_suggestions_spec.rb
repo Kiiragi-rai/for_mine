83dba7da039e56cbe74d630f7676944a03b252ea
rai-h-6 <laixixigu59@gmail.com>
2026-03-29 20:59:49 +0900

    systemspec

--- spec/requests/gift_suggestions_spec.rb
+++ spec/requests/gift_suggestions_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "Partners", type: :request do
   let(:user) { create(:user) }
-  let!(:partner) { create(:partner, user: user) }
+  
 
   before do
     sign_in user
@@ -10,11 +10,24 @@ RSpec.describe "Partners", type: :request do
   end
 
 
-  describe "POST /gift_suggestions" do
+  describe "プレゼント提案（パートナー登録あり）" do
+    let!(:partner) { create(:partner, user: user) }
+    context "パートナー登録済み" do
     it "提案できる" do
       expect {
         post gift_suggestions_path
       }.to change(GiftSuggestion, :count).by(1)
+    end      
+    end
+  end
+
+  describe "プレゼント提案（パートナー登録あり）" do
+    context "パートナー未登録" do
+    it "提案できない" do
+      expect {
+        post gift_suggestions_path
+      }.not_to change(GiftSuggestion, :count)
+    end      
     end
   end
 end
--- spec/systems/user_spec.rb
+++ spec/systems/user_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe "Users", type: :system do
+  let(:user) { create(:user) }
+  describe 'ログイン前' do
+    context "記念日ページへ遷移" do
+      it "記念日ページアクセス失敗" do
+        visit anniversaries_path
+        expect(page). to have_content("You need to sign in or sign up before continuing.")
+        expect(current_path).to eq root_path
+      end
+    end
+  end
   describe 'ログイン後' do
     before do
       visit root_path

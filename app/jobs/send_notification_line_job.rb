faee319cf8e655c7dbdd4b1dace9e6d0f7d17dbc
rai-h-6 <laixixigu59@gmail.com>
2026-03-30 00:38:12 +0900

    cicd調整

--- app/jobs/send_notification_line_job.rb
+++ app/jobs/send_notification_line_job.rb
@@ -7,7 +7,7 @@ class SendNotificationLineJob < ApplicationJob
   # 送信service
   def perform(management_id:)
   Rails.logger.info "ここからsend notificaitonline jobだよん"    # Do something later
-    notification_management = NotificationManagement.find_by(id:management_id)
+    notification_management = NotificationManagement.find_by(id: management_id)
     return if notification_management.success?
 
     notification_setting = notification_management.notification_setting

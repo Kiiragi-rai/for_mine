d48b1d1724d7cf9ed78bd488517e17fc92d29c75
rai-h-6 <laixixigu59@gmail.com>
2026-04-04 14:25:28 +0900

    rubocop追加

--- app/services/line_notification/notification_get.rb
+++ app/services/line_notification/notification_get.rb
@@ -1,7 +1,7 @@
 module LineNotification
   class NotificationGet
     def self.setting
-      # transaction必要ないかも、user_id, start_on, end_on , いらない
+        # transaction必要ないかも、user_id, start_on, end_on , いらない
 
         sql= <<~SQL
 
@@ -68,7 +68,7 @@ module LineNotification
         AnniversaryNotificationTarget.new(row.to_h)
       end
       end
-    end
+  end
 end
 # EXTRACT(HOUR FROM ns.notification_time) =EXTRACT(HOUR FROM b.next_hour)
 # ns.notification_time = date_trunc('hour', b.next_hour)::time
@@ -112,71 +112,71 @@ end
 
 
 
-    # def testsetting
-    #   # transaction必要ないかも、user_id, start_on, end_on , いらない
+# def testsetting
+#   # transaction必要ないかも、user_id, start_on, end_on , いらない
 
-    #     sql= <<~SQL
+#     sql= <<~SQL
 
-    #       WITH base AS (
-    #         SELECT
-    #           date_trunc(
-    #             'hour',
-    #             now() + interval '2 hour'
-    #           )AS next_hour
-    #       )
-    #     SELECT
-    #       u.id AS user_id,
-    #       ns.id AS notification_setting_id,
-    #       a.title,
-    #       b.next_hour AS scheduled_for
+#       WITH base AS (
+#         SELECT
+#           date_trunc(
+#             'hour',
+#             now() + interval '2 hour'
+#           )AS next_hour
+#       )
+#     SELECT
+#       u.id AS user_id,
+#       ns.id AS notification_setting_id,
+#       a.title,
+#       b.next_hour AS scheduled_for
 
-    #       FROM anniversaries a
-    #       INNER JOIN users u
-    #       ON a.user_id = u.id
+#       FROM anniversaries a
+#       INNER JOIN users u
+#       ON a.user_id = u.id
 
-    #       INNER JOIN notification_settings ns
-    #       ON a.id = ns.anniversary_id
+#       INNER JOIN notification_settings ns
+#       ON a.id = ns.anniversary_id
 
-    #       CROSS JOIN base b
+#       CROSS JOIN base b
 
-    #       LEFT JOIN notification_managements nm
-    #       ON ns.id = nm.notification_setting_id
-    #       AND nm.scheduled_for = b.next_hour
+#       LEFT JOIN notification_managements nm
+#       ON ns.id = nm.notification_setting_id
+#       AND nm.scheduled_for = b.next_hour
 
-    #     #{'  '}
+#     #{'  '}
 
-    #       WHERE
-    #         ns.is_enabled = true
+#       WHERE
+#         ns.is_enabled = true
 
-    #         AND
-    #         nm.id IS NULL
+#         AND
+#         nm.id IS NULL
 
-    #         AND
-    #         (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date between ns.start_on and ns.end_on
+#         AND
+#         (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date between ns.start_on and ns.end_on
 
-    #         AND
-    #         ns.notification_hour = EXTRACT(HOUR FROM (b.next_hour AT TIME ZONE 'Asia/Tokyo'))
+#         AND
+#         ns.notification_hour = EXTRACT(HOUR FROM (b.next_hour AT TIME ZONE 'Asia/Tokyo'))
 
-    #         AND
-    #         (ns.last_sent_on IS NULL
-    #         OR
-    #         ns.frequency_days <= (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date - ns.last_sent_on
-    #         OR#{' '}
-    #         (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date = ns.end_on)
-    #     #{'    '}
-    #         ORDER BY ns.id, u.id
+#         AND
+#         (ns.last_sent_on IS NULL
+#         OR
+#         ns.frequency_days <= (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date - ns.last_sent_on
+#         OR#{' '}
+#         (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date = ns.end_on)
+#     #{'    '}
+#         ORDER BY ns.id, u.id
 
-    #   SQL
-    #   # -- (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date  BETWEEN ns.start_on and ns.end_on
-    #   # -- EXTRACT(HOUR FROM ns.notification_time) =EXTRACT(HOUR FROM (b.next_hour AT TIME ZONE 'Asia/Tokyo'))
+#   SQL
+#   # -- (b.next_hour AT TIME ZONE 'Asia/Tokyo')::date  BETWEEN ns.start_on and ns.end_on
+#   # -- EXTRACT(HOUR FROM ns.notification_time) =EXTRACT(HOUR FROM (b.next_hour AT TIME ZONE 'Asia/Tokyo'))
 
-    #   results = ActiveRecord::Base.connection.exec_query(
-    #     sql,
-    #     "notification_date"
-    #   )
-    #   Rails.logger.info "#{results.to_a} これresultsだよおーーん"
+#   results = ActiveRecord::Base.connection.exec_query(
+#     sql,
+#     "notification_date"
+#   )
+#   Rails.logger.info "#{results.to_a} これresultsだよおーーん"
 
-    #   # results.map do |row|
-    #   #   AnniversaryNotificationTarget.new(row.to_h)
-    #   # end
-    #   end
+#   # results.map do |row|
+#   #   AnniversaryNotificationTarget.new(row.to_h)
+#   # end
+#   end

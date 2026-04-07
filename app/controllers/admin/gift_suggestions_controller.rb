c58b3d7bd7d62d211b72fc1e08aedd342725d3f7
rai-h-6 <laixixigu59@gmail.com>
2026-04-07 16:06:18 +0900

    giftsuggestionにつけ忘れadmin login

--- app/controllers/admin/gift_suggestions_controller.rb
+++ app/controllers/admin/gift_suggestions_controller.rb
@@ -1,4 +1,6 @@
 class Admin::GiftSuggestionsController < Admin::BaseController
+  before_action :authenticate_admin!
+
   def index
     @q = GiftSuggestion.ransack(params[:q])
     @gift_suggestions = @q.result(distinct: true).order(created_at: :asc).page(params[:page]).per(10)
